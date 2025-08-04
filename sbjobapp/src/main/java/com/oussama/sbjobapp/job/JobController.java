a9028f604528b595a434742d32bd1229c9a72abb
blcoussama <belcadioussama.eng@gmail.com>
2025-08-04 22:53:36 +0100

    ResponseEntity Class and HTTP STATUS

--- sbjobapp/src/main/java/com/oussama/sbjobapp/job/JobController.java
+++ sbjobapp/src/main/java/com/oussama/sbjobapp/job/JobController.java
@@ -2,6 +2,8 @@ package com.oussama.sbjobapp.job;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,19 +21,23 @@ public class JobController {
 
     @GetMapping("/jobs")
     // This method returns a list of jobs
-    public List<Job> findAll() {
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job) {
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job created successfully with ID: " + job.getId();
+        return new ResponseEntity<>("Job created successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id) {
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
         Job job = jobService.getJobById(id);
-        return job;
+        if (job != null) {
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
