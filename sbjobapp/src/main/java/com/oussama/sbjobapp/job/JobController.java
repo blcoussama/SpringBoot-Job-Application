5fb801be1fde183ef9e0d72a61b66ca5aa761655
blcoussama <belcadioussama.eng@gmail.com>
2025-08-04 14:19:21 +0100

    GFet Jobs and Add Job Endpoints

--- sbjobapp/src/main/java/com/oussama/sbjobapp/job/JobController.java
+++ sbjobapp/src/main/java/com/oussama/sbjobapp/job/JobController.java
@@ -1,7 +1,27 @@
 package com.oussama.sbjobapp.job;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class JobController {
+
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    // This method returns a list of jobs
     public List<Job> findAll() {
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "Job created successfully with ID: " + job.getId();
     }
 }
